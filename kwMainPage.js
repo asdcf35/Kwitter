01f779f6334f95197156e3f6adcd338f3f48190e
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-12-01 18:40:39 -0600

    cc

---
+++ kwMainPage.js
@@ -0,0 +1,20 @@
+img = document.getElementById('profilePic');
+userName = localStorage.getItem('user');
+img.src=`https://avatars.dicebear.com/api/bottts/${user}.svg`;
+var firebaseConfig = {
+    apiKey: "AIzaSyC9yGzV0lX6VOR9UWcNypekT9mr4d7KLmU",
+    authDomain: "kwitter-5c790.firebaseapp.com",
+    databaseURL: "https://kwitter-5c790-default-rtdb.firebaseio.com",
+    projectId: "kwitter-5c790",
+    storageBucket: "kwitter-5c790.appspot.com",
+    messagingSenderId: "520446487724",
+    appId: "1:520446487724:web:48b2162a3bff7cca594309"
+};
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+
+function aRoom(){
+    userName = document.getElementById("rName").value;
+    
+}
