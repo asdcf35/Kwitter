2b297200d7b1efed6b4ce95bb6344b72b6d32ced
asdcf35 <66645620+asdcf35@users.noreply.github.com>
2021-12-03 16:36:52 -0600

    sdf

--- kwMainPage.js
+++ kwMainPage.js
@@ -1,8 +1,18 @@
+function makeid(length) 
+{
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+}
+return result;
+}
+
 function profilePic(){
 img = document.getElementById('profilePic');
-str = localStorage.getItem('user');
-userName = str.replace(/[^a-zA-Z ]/g, "")
-img.src=`https://avatars.dicebear.com/api/bottts/${userName}.svg`;
+mpx = makeid(11);
+img.src=`https://avatars.dicebear.com/api/bottts/${mpx}.svg`;
 }
 var firebaseConfig = {
     apiKey: "AIzaSyC9yGzV0lX6VOR9UWcNypekT9mr4d7KLmU",
